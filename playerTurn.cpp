a6044dbca33d479644a835e4ffd8eae34de2c74e
Lena <taietart@mail.ru>
2023-09-18 13:10:54 +0300

    fixed reinput from player

--- playerTurn.cpp
+++ playerTurn.cpp
@@ -3,14 +3,18 @@
 
 void playerTurn(int ( &gameField )[3][3]) {
     short x, y;
-    std::cin >> x >> y;
-    if (x < 0 || x > 3 || y < 0 || y > 3) {
-        std::cout << "Введите корректное значение";
+    bool correct_input = false;
+    while (!correct_input) {
+        std::cin >> x >> y;
+        if (x < 0 || x > 3 || y < 0 || y > 3) {
+            std::cout << "Введите корректное значение";
+        }
+        else if (gameField[x][y] != -1) { //TODO: refactor with enum
+            std::cout << "Эта клетка занята";
+        }
+        else {
+            correct_input = true;
+        }
     }
-    else if (gameField[x][y] != -1) { //TODO: refactor with enum
-        std::cout << "Эта клетка занята";
-    }
-    else {
         gameField[x][y] = 1;
-    }
 }
