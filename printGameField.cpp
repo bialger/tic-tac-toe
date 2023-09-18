4f8ab562a9d6e2456696ab9babe6db9f8246f72f
wannabread <idanilamor6@gmail.com>
2023-09-18 15:02:39 +0300

    added printGameField.cpp with tic-tac-toe field output

--- printGameField.cpp
+++ printGameField.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+char findSymb(int x){
+    switch (x){
+        case -1: return ' ';
+        case 0: return 'x';
+        case 1: return 'o';
+        default:
+            return ' ';
+    }
+}
 
 void printGameField(int(&gameField)[3][3]) {
-    char symb[3]={' ', 'x', 'o'};
     for (int i = 0; i < 3; i++) {
         //TODO: refactor the code, also use enum instead of int in gameField
-        std::cout << symb[gameField[i][0]] << '|' << symb[gameField[i][1]] << '|' << symb[gameField[i][2]] << '\n';
+        std::cout << findSymb(gameField[i][0]) << '|' << findSymb(gameField[i][1]) << '|' << findSymb(gameField[i][2]) << '\n';
         if (i != 2) {
             std::cout << '_' << '_' << '_' << '_' << '_' << '\n';
         }
